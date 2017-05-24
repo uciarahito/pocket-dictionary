b2afcb3da4d8aeff857fb7e9427125f39bbc88f0
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-05-24 19:04:31 +0700

    add fitur speech to text

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,11 +5,22 @@ const userController = require('../controllers/userController')
 router.post('/api/signup', userController.signup)
 router.post('/api/signin', userController.signin)
 
+
+
+router.get('/', function(req, res) {
+    res.redirect('/login')
+});
 router.get('/login', function(req, res) {
     res.render('login', {error: null})
 });
 router.get('/register', function(req, res) {
     res.render('register', {success: null})
+});
+router.get('/dashboard', function(req, res) {
+    res.render('dashboard', {error: null})
+});
+router.post('/result', function(req, res) {
+    res.send(req.body.speech)
 });
 
 // 'login', {error: null}
